1c4ae5e91ffabcace0415e4579a5e6784e4b5384
Shivasai Jetla <136343063+shivasai-jetla@users.noreply.github.com>
2025-04-03 22:22:36 +0530

    Update BudgetModel.java

--- Milestone 1/src/main/java/com/example/demo/model/BudgetModel.java
+++ Milestone 1/src/main/java/com/example/demo/model/BudgetModel.java
@@ -3,15 +3,13 @@ package com.example.demo.model;
 public class BudgetModel {
 	private Integer userId;
 	private String category;
-	private double price;
 	private Integer months;
 	private double amount;
 	
-	public BudgetModel(Integer userId, String category, double price, Integer months, double amount) {
+	public BudgetModel(Integer userId, String category, Integer months, double amount) {
 		super();
 		this.userId = userId;
 		this.category = category;
-		this.price = price;
 		this.months = months;
 		this.amount = amount;
 	}
@@ -27,12 +25,6 @@ public class BudgetModel {
 	}
 	public void setCategory(String category) {
 		this.category = category;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
 	}
 	public Integer getMonths() {
 		return months;
